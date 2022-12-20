6cd67cb994bd15cd8fbf5864810246db605906eb
KhvGeorge <83204251+KhvGeorge@users.noreply.github.com>
2022-12-20 10:30:54 -0800

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="bruh" />
+      <Todo text="moth" />
+      <Todo text="sist" />
+    </div>
+  );
+}
+
+export default App;
